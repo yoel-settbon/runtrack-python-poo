0f2b855b7a174dce4ea763828839048f79036085
yoel-settbon <yoel-settbon@laplateforme.io>
2025-02-03 15:41:33 +0100

    :rocket: job02 done

---
+++ jour01/job02.py
@@ -0,0 +1,10 @@
+"""À l’aide de la classe Operation créée au-dessus, imprimez en console la
+valeur des attributs “nombre1” et “nombre2”. Résulat attendu : Le nombre1 est 12 // Le nombre2 est 3"""
+
+
+from job01 import Operation
+
+operation1 = Operation(12, 3)
+
+print(f"Le nombre1 est {operation1.nombre1}")
+print(f"Le nombre2 est {operation1.nombre2}")
